e050e65e9e2e265368a397813bb8343dcf05c379
rossellamorena03-ux <rossella.morena03@gmail.com>
2025-10-23 13:16:22 +0200

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getMessaging, onMessage } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_KEY_FIREBASE,
+  authDomain: import.meta.env.VITE_FIREBASAE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+const db = getFirestore(app);
+export const auth = getAuth(app);
+export { db, messaging, onMessage, app };
