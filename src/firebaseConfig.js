0bef79e44272f9bea8225ef816c775b843fcd1df
rossellamorena03-ux <rossella.morena03@gmail.com>
2025-10-23 15:10:55 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,11 +7,13 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_KEY_FIREBASE,
   authDomain: import.meta.env.VITE_FIREBASAE_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 const db = getFirestore(app);
 export const auth = getAuth(app);
+
 export { db, messaging, onMessage, app };
